a2611dd9c01ca75789dafc0eae75fed2f0e383e5
Xiaoxi-Zhang <96587298+Xiaoxi-Zhang@users.noreply.github.com>
2025-12-03 18:24:59 +0800

    登录拦截+增加进度条

--- src/permission.js
+++ src/permission.js
@@ -1,67 +1,35 @@
-// import router from './router'
-// import store from './store'
-// import { Message } from 'element-ui'
-// import NProgress from 'nprogress' // progress bar
-// import 'nprogress/nprogress.css' // progress bar style
-// import { getToken } from '@/utils/auth' // get token from cookie
-// import getPageTitle from '@/utils/get-page-title'
+// 全局前置守卫做判断
+import router from './router'
+import store from './store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
-// NProgress.configure({ showSpinner: false }) // NProgress Configuration
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  // 从store中获取token
+  const token = store.getters.token
+  if (token) {
+    // token存在
+    if (to.path === '/login') {
+      next('/')
+      NProgress.done()
+    } else {
+      next()
+    }
+  } else {
+    // 没有token
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      next('/login')
+      NProgress.done()
+    }
+  }
+})
 
-// const whiteList = ['/login'] // no redirect whitelist
+// beforeEach中重定向后，不会再走afterEach
+router.afterEach((to, from) => {
+  NProgress.done()
+})
 
-// router.beforeEach(async(to, from, next) => {
-//   // start progress bar
-//   // 加载进度条
-//   NProgress.start()
-
-//   // set page title
-//   // 设置页签的文本
-//   document.title = getPageTitle(to.meta.title)
-
-//   // determine whether the user has logged in
-//   const hasToken = getToken()
-
-//   if (hasToken) {
-//     if (to.path === '/login') {
-//       // if is logged in, redirect to the home page
-//       // next('/')  简单写法
-//       next({ path: '/' }) // 完整写法
-//       NProgress.done()
-//     } else {
-//       const hasGetUserInfo = store.getters.name
-//       if (hasGetUserInfo) {
-//         next()
-//       } else {
-//         try {
-//           // get user info
-//           await store.dispatch('user/getInfo')
-
-//           next()
-//         } catch (error) {
-//           // remove token and go to login page to re-login
-//           await store.dispatch('user/resetToken')
-//           Message.error(error || 'Has Error')
-//           next(`/login?redirect=${to.path}`)
-//           NProgress.done()
-//         }
-//       }
-//     }
-//   } else {
-//     /* has no token*/
-
-//     if (whiteList.indexOf(to.path) !== -1) {
-//       // in the free login whitelist, go directly
-//       next()
-//     } else {
-//       // other pages that do not have permission to access are redirected to the login page.
-//       next(`/login?redirect=${to.path}`)
-//       NProgress.done()
-//     }
-//   }
-// })
-
-// router.afterEach(() => {
-//   // finish progress bar
-//   NProgress.done()
-// })
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -79,7 +79,7 @@ export default {
     return {
       loginForm: {
         mobile: '13800000002',
-        password: '929itheima.CN032@.20251202'
+        password: '929itheima.CN032@.20251203'
       },
       loginRules: {
         mobile: [
