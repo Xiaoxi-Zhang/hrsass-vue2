d93672b29963c0d14fa2979af4d53e4c6915b41b
ZXX <1461923829@qq.com>
2025-12-21 23:46:36 +0800

    搭建角色管理基本结构+封装并渲染角色列表数据

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,19 +1,63 @@
 <template>
   <div class="setting-container">
     <div class="app-container">
-      <h2>
-        公司设置
-      </h2>
+      <el-card>
+        <el-tabs>
+          <!-- 左侧 -->
+          <el-tab-pane label="角色管理">
+            <!-- 按钮 -->
+            <el-button
+              icon="el-icon-plus"
+              size="small"
+              type="primary"
+            >新增角色</el-button>
+            <!-- 表格 -->
+            <el-table :data="list">
+              <el-table-column label="序号" width="100" type="index" />
+              <el-table-column label="角色名称" width="240" prop="name" />
+              <el-table-column label="描述" prop="description" />
+              <el-table-column label="操作">
+                <el-button size="small" type="success">分配权限</el-button>
+                <el-button size="small" type="primary">编辑</el-button>
+                <el-button size="small" type="danger">删除</el-button>
+              </el-table-column>
+            </el-table>
+          </el-tab-pane>
+
+          <el-tab-pane label="公司信息">
+            <!-- 公司信息 -->
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import { getRoleListAPI } from '@/api/setting'
+
 export default {
-  name: 'Setting'
+  name: 'Setting',
+  data() {
+    return {
+      page: 1,
+      pagesize: 10,
+      list: []
+    }
+  },
+  created() {
+    this.getRoleList()
+  },
+  methods: {
+    async getRoleList() {
+      const res = await getRoleListAPI(this.page, this.pagesize)
+      // console.log(res)
+      this.list = res.data.rows
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 
 </style>
