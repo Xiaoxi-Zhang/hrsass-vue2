b38e9217aa145aabcf9bf439be4295829fa07609
ZXX <1461923829@qq.com>
2025-12-24 23:56:44 +0800

    具名插槽优化

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="employees-container">
     <div class="app-container">
-      <PageTools />
+      <PageTools>
+        <template #left>
+          共有100条数据
+        </template>
+        <template #right>
+          <el-button type="primary">excel导入</el-button>
+          <el-button type="primary">excel导出</el-button>
+        </template>
+      </PageTools>
     </div>
   </div>
 </template>
