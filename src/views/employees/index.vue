4f0ec5814f801ff385b3fa53dc82c1eff0373cb8
Xiaoxi-Zhang <96587298+Xiaoxi-Zhang@users.noreply.github.com>
2025-12-25 18:38:40 +0800

    优化聘用形式的展示格式

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -11,13 +11,13 @@
           <el-button type="primary" size="small">新增员工</el-button>
         </template>
       </page-tools>
-      <el-card style="margin-top: 10px;">
+      <el-card v-loading="isLoading" style="margin-top: 10px">
         <el-table border :data="list">
-          <el-table-column label="序号" />
+          <el-table-column label="序号" type="index" :index="indexMethod" width="100" />
           <el-table-column label="姓名" prop="username" />
           <el-table-column label="手机号" prop="mobile" />
           <el-table-column label="工号" prop="workNumber" />
-          <el-table-column label="聘用形式" prop="formOfEmployment" />
+          <el-table-column label="聘用形式" prop="formOfEmployment" :formatter="formateText" />
           <el-table-column label="部门" prop="departmentName" />
           <el-table-column label="入职时间" prop="timeOfEntry" />
           <el-table-column label="操作" fixed="right" width="280">
@@ -30,7 +30,14 @@
         </el-table>
         <!-- 分页组件 -->
         <div style="height: 60px; margin-top: 10px">
-          <el-pagination layout="prev, pager, next" />
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :current-page="page"
+            :page-size="size"
+            :total="total"
+            @current-change="handleCurrentChange"
+          />
         </div>
       </el-card>
     </div>
@@ -39,6 +46,7 @@
 
 <script>
 import { getEmployeeListAPI } from '@/api/employees'
+import EnumObj from '@/constant/employees'
 
 export default {
   name: 'Employees',
@@ -47,22 +55,43 @@ export default {
       page: 1,
       size: 10,
       total: 0,
-      list: []
+      list: [],
+      isLoading: false
     }
   },
   created() {
     this.getEmployeeList()
   },
   methods: {
+    formateText(row, column, cellValue, index) {
+      // console.log(row, column, cellValue, index)
+      const formOfEmployment = +cellValue
+      // if (formOfEmployment === 1) {
+      //   return '正式'
+      // } else if (formOfEmployment === 2) {
+      //   return '非正式'
+      // }
+      const hireType = EnumObj.hireType
+      // find 筛选出符合条件的第一个元素 或者 undefined
+      const result = hireType.find(item => item.id === formOfEmployment)
+      return result ? result.value : '未知'
+    },
+    indexMethod(index) {
+      return index + 1 + this.size * (this.page - 1)
+    },
+    handleCurrentChange(val) {
+      this.page = val
+      this.getEmployeeList()
+    },
     async getEmployeeList() {
+      this.isLoading = true
       const { data: { rows, total }} = await getEmployeeListAPI(this.page, this.size)
       this.total = total
       this.list = rows
+      this.isLoading = false
     }
   }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
