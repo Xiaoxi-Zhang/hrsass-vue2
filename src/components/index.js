c4e87c2e2403825b8384f415ab0a9850e80cd724
Xiaoxi-Zhang <96587298+Xiaoxi-Zhang@users.noreply.github.com>
2025-12-25 15:32:13 +0800

    员工模块基本结构渲染

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+// 下边这行代码就声明了一个Vue中插件
+import PageTools from '@/components/PageTools'
+// export default {
+//   // install方法中有一个形参，形参就是Vue构造函数
+//   // 插件的强大之处：关于在Vue构造函数中的操作 都可以在install方法中实现
+//   // Vue.component  Vue.prototype  Vue.directive
+//   install(Vue) {
+//     Vue.component('PageTools', PageTools)
+//   }
+// }
+
+// 如果插件是一个函数，函数的形参中就可以接受Vue构造函数
+export default function(Vue) {
+  Vue.component('PageTools', PageTools)
+}
+
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,22 +1,65 @@
 <template>
   <div class="employees-container">
     <div class="app-container">
-      <PageTools>
+      <page-tools>
         <template #left>
-          共有100条数据
+          <span>总记录数: {{ total }} 条</span>
         </template>
         <template #right>
-          <el-button type="primary">excel导入</el-button>
-          <el-button type="primary">excel导出</el-button>
+          <el-button type="warning" size="small">excel导入</el-button>
+          <el-button type="danger" size="small">excel导出</el-button>
+          <el-button type="primary" size="small">新增员工</el-button>
         </template>
-      </PageTools>
+      </page-tools>
+      <el-card style="margin-top: 10px;">
+        <el-table border :data="list">
+          <el-table-column label="序号" />
+          <el-table-column label="姓名" prop="username" />
+          <el-table-column label="手机号" prop="mobile" />
+          <el-table-column label="工号" prop="workNumber" />
+          <el-table-column label="聘用形式" prop="formOfEmployment" />
+          <el-table-column label="部门" prop="departmentName" />
+          <el-table-column label="入职时间" prop="timeOfEntry" />
+          <el-table-column label="操作" fixed="right" width="280">
+            <template>
+              <el-button type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">分配角色</el-button>
+              <el-button type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页组件 -->
+        <div style="height: 60px; margin-top: 10px">
+          <el-pagination layout="prev, pager, next" />
+        </div>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import { getEmployeeListAPI } from '@/api/employees'
+
 export default {
-  name: 'Employees'
+  name: 'Employees',
+  data() {
+    return {
+      page: 1,
+      size: 10,
+      total: 0,
+      list: []
+    }
+  },
+  created() {
+    this.getEmployeeList()
+  },
+  methods: {
+    async getEmployeeList() {
+      const { data: { rows, total }} = await getEmployeeListAPI(this.page, this.size)
+      this.total = total
+      this.list = rows
+    }
+  }
 }
 </script>
 
