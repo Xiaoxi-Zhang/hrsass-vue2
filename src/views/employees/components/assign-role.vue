d37850bfd0a76b2595f933f7487d898b46bba257
Xiaoxi-Zhang <96587298+Xiaoxi-Zhang@users.noreply.github.com>
2026-01-15 16:06:44 +0800

    回显基本的角色信息

--- src/views/employees/components/assign-role.vue
+++ src/views/employees/components/assign-role.vue
@@ -1,7 +1,18 @@
 <template>
-  <el-dialog class="assign-role" title="分配角色" :visible="showRoleDialog" :close-on-click-modal="false" @close="closeRoleDialog">
+  <el-dialog
+    class="assign-role"
+    title="分配角色"
+    :visible="showRoleDialog"
+    :close-on-click-modal="false"
+    @close="closeRoleDialog"
+    @open="openDialog"
+  >
     <!-- 这里准备复选框 -->
-    ...
+    <el-checkbox-group v-model="roleIds">
+      <el-checkbox v-for="item in roleList" :key="item.id" :label="item.id">
+        {{ item.name }}
+      </el-checkbox>
+    </el-checkbox-group>
 
     <template #footer>
       <el-button type="primary" size="small">确定</el-button>
@@ -11,6 +22,9 @@
 </template>
 
 <script>
+import { getRoleListAPI } from '@/api/setting'
+import { getUserAvatarAPI } from '@/api/user'
+
 export default {
   props: {
     showRoleDialog: {
@@ -23,7 +37,28 @@ export default {
       default: null
     }
   },
+  data() {
+    return {
+      roleIds: [],
+      roleList: []
+    }
+  },
   methods: {
+    openDialog() {
+      this.getRoleList()
+      this.getUserRole()
+    },
+    // 获取用户原本的角色信息
+    async getUserRole() {
+      const { data: { roleIds }} = await getUserAvatarAPI(this.userId)
+      // 当roleIds为null的时候，默认让他是一个空数组[]
+      this.roleIds = roleIds || []
+    },
+    // 获取所有的角色列表
+    async getRoleList() {
+      const { data: { rows }} = await getRoleListAPI(1, 99999)
+      this.roleList = rows
+    },
     // 关闭弹框
     closeRoleDialog() {
       this.$emit('update:showRoleDialog', false)
