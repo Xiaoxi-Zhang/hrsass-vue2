4596164c516d70fc9b887973c8d7650b7532ad69
Xiaoxi-Zhang <96587298+Xiaoxi-Zhang@users.noreply.github.com>
2026-01-22 15:29:56 +0800

    处理下拉菜单与日历的联动关系

--- src/views/dashboard/components/work-calendar.vue
+++ src/views/dashboard/components/work-calendar.vue
@@ -2,14 +2,16 @@
   <div>
     <!-- 工作日历, 年和月 -->
     <div class="select-box">
-      <el-select v-model="currentYear" size="small" style="width: 120px; margin-right: 10px">
+      <el-select v-model="currentYear" size="small" style="width: 120px; margin-right: 10px" @change="changeDate">
         <!-- 年份取给定年份的, 前五年 + 后五年 -->
         <el-option v-for="item in yearList" :key="item" :label="item" :value="item" />
       </el-select>
-      <el-select v-model="currentMonth" size="small" style="width: 120px;">
+      <el-select v-model="currentMonth" size="small" style="width: 120px;" @change="changeDate">
         <el-option v-for="item in 12" :key="item" :label="item" :value="item" />
       </el-select>
     </div>
+    <!-- el-calendar跟select-box是同级的元素 -->
+    <el-calendar v-model="currentDate" />
   </div>
 </template>
 
@@ -26,7 +28,8 @@ export default {
   data() {
     return {
       currentYear: null, // 当前年份
-      currentMonth: null // 当前月份
+      currentMonth: null, // 当前月份
+      currentDate: null
     }
   },
   computed: {
@@ -46,6 +49,11 @@ export default {
   created() {
     this.currentYear = this.startDate.getFullYear() // 得到当前年份
     this.currentMonth = this.startDate.getMonth() + 1 // 当前月份
+  },
+  methods: {
+    changeDate() {
+      this.currentDate = new Date(`${this.currentYear}-${this.currentMonth}-1`)
+    }
   }
 }
 </script>
@@ -54,5 +62,47 @@ export default {
 .select-box {
   display: flex;
   justify-content: flex-end;
+}
+
+::v-deep .el-calendar-day {
+  height:  auto;
+}
+::v-deep .el-calendar-table__row td::v-deep .el-calendar-table tr td:first-child, ::v-deep .el-calendar-table__row td.prev{
+  border: none;
+}
+.date-content {
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+  font-size: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.date-content .rest {
+  color: #fff;
+  border-radius: 50%;
+  background: rgb(250, 124, 77);
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+  display: inline-block;
+  font-size: 12px;
+  margin-left: 10px;
+}
+.date-content .text{
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+  display: inline-block;
+
+}
+::v-deep .el-calendar-table td.is-selected .text{
+  background: #409eff;
+  color: #fff;
+  border-radius: 50%;
+}
+::v-deep .el-calendar__header {
+  display: none
 }
 </style>
